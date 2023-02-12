44ee717c669a7e134f324b11713281de53c48f67
Vyacheslav Kulikov <slavonmail10@gmail>
2023-02-12 18:16:24 +0200

    added 8 tasks, solved tasc2

---
+++ task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите число 1: ");
+int a =int.Parse(Console.ReadLine()!);
+Console.Write("Введите число 2: ");
+int b =int.Parse(Console.ReadLine()!);
+
+if (a > b)
+{
+    System.Console.WriteLine(a + " Наебольшее число");
+    System.Console.WriteLine(b + " Наеменьшее число");
+}
+if (a < b)
+{
+    System.Console.WriteLine(a + " Наеменьшее число");
+    System.Console.WriteLine(b + " Наебольшее число");
+}
+if (a == b)
+{
+    System.Console.WriteLine("Числа равны");
+}
+
+// if (a < b)
+// {
+//     System.Console.WriteLine(b + " Наебольшее число");
+// }
+// if (a > b)
+// {
+//     System.Console.WriteLine(b + " Наеменьшее число");
+// }
---
+++ task3/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.Write("Ведите число от 1-7: ");
+int number =int.Parse(Console.ReadLine()!);
+string mon = "Понедельник";
+string tue = "Вторник";
+string wed = "Среда";
+string thu = "Четверг";
+string fri = "Пятница";
+string sat = "Суббота";
+string sun = "Васкресенье";
+
+string day = "В неделе всего 7 дней";  
+
+if (number == 1) day = mon;
+if (number == 2) day = tue;
+if (number == 3) day = wed;
+if (number == 4) day = thu;
+if (number == 5) day = fri;
+if (number == 6) day = sat;
+if (number == 7) day = sun;
+
+Console.Write("День недели: ");
+Console.WriteLine(day);
+
+/*************************Второй вариант*******************************/
+
+// Console.Write("Введите число от 1-7: ");
+// int number = int.Parse(Console.ReadLine()!);
+
+// if (number == 1)
+// {
+//     System.Console.WriteLine("Понедельник");
+// }
+
+// if (number == 2)
+// {
+//     System.Console.WriteLine("Вторник");
+// }
+
+// if (number == 3)
+// {
+//     System.Console.WriteLine("Среда");
+// }
+
+// if (number == 4)
+// {
+//     System.Console.WriteLine("Четверг");
+// }
+
+// if (number == 5)
+// {
+//     System.Console.WriteLine("Пятница");
+// }
+
+// if (number == 6)
+// {
+//     System.Console.WriteLine("Суббота");
+// }
+
+// if (number == 7)
+// {
+// System.Console.WriteLine("Воскресенье");
+// }
