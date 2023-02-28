0c51b13e5c16800f38f3df11fca16cf517653253
Vyacheslav Kulikov <slavonmail10@gmail>
2023-02-28 10:51:38 +0200

    Исправил домашку3

--- lesson4/README.md
+++ lesson4/README.md
@@ -10,6 +10,6 @@
 
 ## Домашнее задание
 
-* [task4](https://github.com/KulSlavOn/C-seminars-lessons/tree/main/lesson4/task4)
 * [task5](https://github.com/KulSlavOn/C-seminars-lessons/tree/main/lesson4/task5)
 * [task6](https://github.com/KulSlavOn/C-seminars-lessons/tree/main/lesson4/task6)
+* [task7](https://github.com/KulSlavOn/C-seminars-lessons/tree/main/lesson4/task7)
--- lesson4/task5/Program.cs
+++ lesson4/task5/Program.cs
@@ -5,3 +5,27 @@
 
 // 2, 4 -> 16
 
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+        // Console.Write(array[i]+" ");
+    }
+}
+
+void PrintArray(int[] mas)
+{
+    int len = mas.Length;
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write(mas[i] + " ");
+    }
+}
+
+int[] arr = new int[8];
+Console.Clear();
+FillArray(arr);
+// Console.WriteLine();
+PrintArray(arr);
