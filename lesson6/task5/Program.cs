8186cbce241226cae9553662174f10bf4ef7c91b
Vyacheslav Kulikov <slavonmail10@gmail>
2023-03-13 22:04:42 +0200

    Решил дз уроку 6

--- lesson6/task5/Program.cs
+++ lesson6/task5/Program.cs
@@ -5,3 +5,72 @@
 
 // 1, -7, 567, 89, 223-> 3
 
+void CountPosNum()
+{
+    // Запрашиваем у пользователя количество чисел для ввода
+    Console.Write("Введите количество чисел для ввода: ");
+    int count = int.Parse(Console.ReadLine()!);
+
+    // Считываем числа и считаем количество положительных чисел
+    int countPositive = 0;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"Введите число {i + 1}: ");
+        int num = int.Parse(Console.ReadLine()!);
+
+        if (num > 0)
+        {
+            countPositive++;
+        }
+    }
+
+    // Выводим результат на консоль
+    Console.WriteLine($"Количество положительных чисел: {countPositive}");
+}
+
+CountPosNum();
+
+// int Prompt(string message)
+// {
+//     System.Console.Write(message);
+//     string value = Console.ReadLine()!;
+//     int result = Convert.ToInt32(value);
+//     return result;
+// }
+
+// int[] InputArray(int length)
+// {
+//     int[] array = new int[length];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = Prompt($"Введите {i + 1}-й элимент ");
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for(int i =0; i<array.Length; i++)
+//     {
+//         Console.WriteLine($"a [{i}] = {array[i]}");
+//     }
+// }
+
+// int CountPosNum(int[] array)
+// {
+//     int count =0;
+//     for (int i = 0; i< array.Length; i++)
+//     {
+//         if(array[i]>0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// int length = Prompt("Введите количество элементов >>> ");
+// int[] array;
+// array=InputArray(length);
+// PrintArray(array);
+// Console.WriteLine($"Количество чисел больше 0 - {CountPosNum(array)}");
--- lesson6/task6/Program.cs
+++ lesson6/task6/Program.cs
@@ -3,3 +3,30 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void IntersectionPoint()
+{
+    Console.WriteLine("Введите значения k1, b1, k2 и b2 для двух прямых:");
+    double b1 = ReadDouble("b1");
+    double k1 = ReadDouble("k1");
+    double b2 = ReadDouble("b2");
+    double k2 = ReadDouble("k2");
+
+    double x = FindIntersectionX(k1, b1, k2, b2);
+    double y = k1 * x + b1;
+
+    Console.WriteLine($"Точка пересечения: ({x}, {y})");
+}
+
+double ReadDouble(string name)
+{
+    Console.Write($"Введите значение {name}: ");
+    return double.Parse(Console.ReadLine()!);
+}
+
+double FindIntersectionX(double k1, double b1, double k2, double b2)
+{
+    return (b2 - b1) / (k1 - k2);
+}
+
+IntersectionPoint();
